4d4f271d3234da66af14fa79a27bc778ea0355c6
Brett C. Dudo <brett@dudo.io>
2021-08-13 16:49:36 -0700

    Eh...

--- spec/rails_respond_to_rpc_spec.rb
+++ spec/rails_respond_to_rpc_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe RailsRespondToRpc do
     expect(RailsRespondToRpc::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
+  xit 'does something useful' do
     expect(false).to eq(true)
   end
 end
