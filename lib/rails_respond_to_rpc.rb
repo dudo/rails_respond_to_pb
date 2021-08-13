35ad37c6aaf9352f6221cda1adccf3c228ac8d64
Brett C. Dudo <brett@dudo.io>
2021-08-13 16:47:14 -0700

    Cop

--- lib/rails_respond_to_rpc.rb
+++ lib/rails_respond_to_rpc.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "rails_respond_to_rpc/version"
+require_relative 'rails_respond_to_rpc/version'
 
 module RailsRespondToRpc
   class Error < StandardError; end
