1016351c0f4b87786498ffde6597c3229edb8a5d
Brett C. Dudo <brett@dudo.io>
2021-08-17 20:20:55 -0700

    Test Railtie

--- lib/rails_respond_to_pb.rb
+++ lib/rails_respond_to_pb.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require_relative 'rails_respond_to_pb/version'
-require_relative 'rails_respond_to_pb/mime_types' if defined?(Rails)
-require_relative 'rails_respond_to_pb/railtie' if defined?(Rails)
+
+if defined?(Rails)
+  require_relative 'rails_respond_to_pb/mime_types'
+  require_relative 'rails_respond_to_pb/railtie'
+end
 
 module RailsRespondToPb
 end
--- lib/rails_respond_to_pb/railtie.rb
+++ lib/rails_respond_to_pb/railtie.rb
@@ -4,8 +4,18 @@ require_relative './respond_to_pb'
 
 module RailsRespondToPb
   class Railtie < Rails::Railtie
-    initializer 'respond_to_pb' do |app|
-      app.middleware.use(RespondToRpc)
+    initializer 'respond_to_pb.use_middleware' do |app|
+      app.middleware.use(RespondToPb)
+    end
+
+    initializer 'respond_to_pb.controller_include' do
+      ActiveSupport.on_load(:action_controller) do
+        ActionController::Base.include ActionController::MimeResponds
+      end
+    end
+
+    initializer 'respond_to_pb.require_pb' do
+      Dir["#{Rails.root}/lib/**/*_twirp.rb"].each { |file| require file }
     end
   end
 end
