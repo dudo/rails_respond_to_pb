785b36de8b80798f81c7e0465faa9ad0e26b20a6
Brett C. Dudo <brett@dudo.io>
2021-08-17 19:27:45 -0700

    Ensure Rails is loaded

--- lib/rails_respond_to_pb.rb
+++ lib/rails_respond_to_pb.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'rails_respond_to_pb/version'
-require_relative 'rails_respond_to_pb/mime_types'
-require_relative 'rails_respond_to_pb/railtie'
+require_relative 'rails_respond_to_pb/mime_types' if defined?(Rails)
+require_relative 'rails_respond_to_pb/railtie' if defined?(Rails)
 
 module RailsRespondToPb
 end
